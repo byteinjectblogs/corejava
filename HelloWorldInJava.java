124fdd2f198d5ca4ce8cd319bbf75a451df29c3a
byteinjectblogs <132078696+byteinjectblogs@users.noreply.github.com>
2023-04-28 20:28:17 +0530

    Add files via upload
    Hello World Program in java

---
+++ HelloWorldInJava.java
@@ -0,0 +1,11 @@
+//HelloWorldInJava.java
+
+public class HelloWorldInJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!"); // Prints Hello World! to the console.
+
+	}
+
+}
